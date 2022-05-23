efa412e591181f20f5bcfce9305f1a340a41d260
anki palviainen <anki.palviainen@gmail.com>
2022-05-23 14:14:23 +0300

    Create backend for singup/signin

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -12,14 +14,106 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password : {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const User = mongoose.model('User', UserSchema)
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization')
+
+  try {
+    const user = await User.findOne({ accessToken })
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({ response: 'Please log in', success: false })
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+}
+
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Technigo!");
+// });
+
+app.get('/thoughts', authenticateUser)
+app.get('/thoughts', (req, res) => {
+  res.send('Here are your thoughts')
+})
+
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const salt = bcrypt.genSaltSync()
+
+    if (password.length < 5) {
+      throw 'Password must be at least 5 characters long'
+    }
+
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt)
+    }).save()
+
+    res.status(201).json({
+      response: {
+        userId: newUser._id,
+        username: newUser.username,
+        accessToken: newUser.accessToken
+      },
+      success: true
+    })
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+})
+
+app.post('/signin', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await User.findOne({ username })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        response: {
+          userId: user._id,
+          username: user.username,
+          accessToken: user.accessToken
+        },
+        success: true
+      })
+    } else {
+      res.status(404).json({
+        response: "Username or password doesn't match",
+        success: false
+      })
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+})
 
 // Start the server
 app.listen(port, () => {
