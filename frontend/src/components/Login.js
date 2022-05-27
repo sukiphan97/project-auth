7e531fe5d70ce351113a3a7d74ddc49387e409ff
Suki Nhung Phan <nhungphan@Joel-Lov.local>
2022-05-27 13:17:47 +0300

    add styling, new routes

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, batch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import user from "reducers/user";
 import { API_URL } from "ultils/api"; 
 import { useNavigate } from "react-router-dom";
+import styled from 'styled-components';
+import background from '../img/background.png'
 
 const Login = () => {
 
@@ -19,7 +20,6 @@ const Login = () => {
 
     useEffect(() => {
         if (mode === 'signup') {
-            setUsername('')
         }
     },[mode]);
 
@@ -64,39 +64,106 @@ const Login = () => {
     }
 
     return (
-        <>
-            <label htmlFor="signup">Sign up</label>
-            <input 
-                type='radio'
-                id="signup"
-                checked={ mode === 'signup' }
-                onChange={() => setMode('signup')}
-            />
-            <label htmlFor="signin">Sign in</label>
-            <input 
-                type='radio'
-                id="signin"
-                checked={ mode === 'signin' }
-                onChange={() => setMode('signin')}
-
-            />
+        <Container>
+            <Heading>Welcome back</Heading>
+            <SubHeading>
+                Sign in and start sharing your 
+                happy thoughts today
+            </SubHeading>           
             <p>{error}</p>
             <form onSubmit={onFormSubmit}>
-                <label htmlFor="username">User name</label>
-                <input id="username" 
+                <Input id="username" 
                     type='text' 
                     value={username} 
-                    onChange={(e) => setUsername(e.target.value)}/>
-                <label htmlFor="password">Password</label>
-                <input id="password" 
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder = 'Email'
+                />
+                <Input id="password" 
                     type='password' 
                     value={password} 
-                    onChange={(e) => setPassword(e.target.value)}  />
-                <button type="submit">Login</button>
-                <Link to='/'>Here</Link>
+                    onChange={(e) => setPassword(e.target.value)}  
+                    placeholder = 'Password'
+                />
+                <Button type="submit">{mode === 'signin' ? 'Sign in' : 'Sign up'}</Button>
             </form>
-        </>
+            <Mode>{mode === 'signin' ? 'Not a member ? ' : 'Already member ? '} 
+                <button onClick={() => setMode('signup')}>{mode === 'signin' ? 'Sign up' : 'Sign in'}</button>
+            </Mode>         
+        </Container>
     )
 }
 
 export default Login
+
+
+const Container = styled.div`
+    background: url(${background});
+    background-size: cover;
+    background-repeat: no-repeat;
+    min-height: 100vh;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 200px 30px 0 30px;
+
+`
+
+const Heading = styled.p`
+    font-size: 24px;
+    font-weight: 700;
+    letter-spacing: 0.5px;
+    color: #fff;
+    margin-bottom: 15px;
+`
+
+const SubHeading = styled.p`
+    font-size: 16px;
+    color: #eee;
+    max-width: 30ch;
+    line-height: 20px;
+    letter-spacing: 0.5px;
+    text-align: center;
+    margin-bottom: 30px;
+
+`
+const Form = styled.form`
+`
+const Input = styled.input`
+    display: block;
+    width: 310px;
+    height: 45px;
+    padding: 5px;
+    color: #000;
+    border: none;
+    outline: none;
+    font-size: 16px;
+    margin-bottom: 30px;
+    border-radius: 5px;
+`
+const Button = styled.button`
+    display: block;
+    width: 310px;
+    height: 45px;
+    padding: 5px;
+    font-size: 18px;
+    font-weight: 600;
+    color: #fff;
+    border: none;
+    background-color: #FF8A80;
+    border-radius: 5px;
+    margin-top: 50px;
+`
+const Mode = styled.p`
+    font-size: 18px;
+    margin-top: 30px;
+    color: #eee;
+
+   button {
+       color: #FF8A80;
+       background: none;
+       border: none;
+       font-size: 18px;
+       font-weight: 600;
+   }
+`
